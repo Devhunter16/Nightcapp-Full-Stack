f7b1dab83a20b1a20cb4346dd78580713b83426e
Devin Hunter <devhunter16@gmail.com>
2023-12-01 12:24:15 -0700

    Initial backend commit.

---
+++ backend/app.js
@@ -0,0 +1,49 @@
+// Helps enforce a stricter set of rules and coding standards in this file
+"use strict";
+
+/** Express app for nightcapp */
+const express = require("express");
+// CORS is important because it enforces same-origin policy which is a security 
+// measure that prevents a malicious script from accessing resources that it should
+// not have access to
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+const { authenticateJWT } = require("./middleware/auth");
+
+const cocktailDbRoutes = require("./routes/cocktailDb");
+// const authRoutes = require("./routes/auth");
+
+const morgan = require("morgan");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("tiny"));
+
+process.on("unhandledRejection", (reason, promise) => {
+    console.error("Unhandled Rejection at:", promise, "reason:", reason);
+});
+
+// Use the routes AFTER the middlewares
+app.use('/', cocktailDbRoutes);
+
+// app.use('/auth', authRoutes);  
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
