941c08f20a55ec44bd242940fb0e81045689a4f4
Devin Hunter <devhunter16@gmail.com>
2023-12-02 16:23:58 -0700

    Made small changes to app.js.

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,23 @@
 /** Express app for nightcapp */
 const express = require("express");
-// CORS is important because it enforces same-origin policy which is a security 
-// measure that prevents a malicious script from accessing resources that it should
-// not have access to
+// CORS is important because our backend and frontend are running on different ports
 const cors = require("cors");
+
+const cocktailDbRoutes = require("./routes/cocktailDbRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/", cocktailDbRoutes);
 
-/** Handle 404 errors -- this matches everything */
+/** Handle 404 errors */
 app.use(function (req, res, next) {
-    return next(new NotFoundError());
+    res.status(404).json({ error: "Not Found" });
 });
 
-/** Generic error handler; anything unhandled goes here. */
+/** Generic error handler; this middleware should log anything that hits it to the console */
 app.use(function (err, req, res, next) {
     if (process.env.NODE_ENV !== "test") console.error(err.stack);
     const status = err.status || 500;
