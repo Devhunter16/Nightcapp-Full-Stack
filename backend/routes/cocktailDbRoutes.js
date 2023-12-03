af73ef1e0f273ba7bd2f250a3c11f16461d8e908
Devin Hunter <devhunter16@gmail.com>
2023-12-02 17:10:34 -0700

    Added another route to this file.

--- backend/routes/cocktailDbRoutes.js
+++ backend/routes/cocktailDbRoutes.js
@@ -2,20 +2,29 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
-console.log("Route file is being executed");
-
-router.get("/search", async (req, res, next) => {
-    const { recipe } = req.query;
-    console.log("Searching by name:", recipe);
-
-    if (!recipe) {
-        console.log("No recipe input");
+router.get("/search_by_name", async (req, res, next) => {
+    const { name } = req.query;
+    if (!name) {
+        console.log("No cocktail name input");
         // return next(new BadRequestError("You must provide a cocktail name to search."));
     };
-
     try {
-        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${recipe}`);
-        console.log("CocktailDB response:", response.data);
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        return res.json(response.data);
+    } catch (err) {
+        console.error(err);
+        // return next(new NotFoundError("Cocktails not found."));
+    };
+});
+
+router.get("/search_by_ingredient", async (req, res, next) => {
+    const { ingredient } = req.query;
+    if (!ingredient) {
+        console.log("No cocktail ingredient input");
+        // return next(new BadRequestError("You must provide a cocktail name to search."));
+    };
+    try {
+        const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
         return res.json(response.data);
     } catch (err) {
         console.error(err);
