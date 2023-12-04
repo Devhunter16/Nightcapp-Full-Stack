26166ada63aa509c15d6620544811eced9142165
Devin Hunter <devhunter16@gmail.com>
2023-12-04 08:56:56 -0700

    Updated the function in this file to interact with my backend API.

--- frontend/pages/api/randomCocktail.js
+++ frontend/pages/api/randomCocktail.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
+const BACKEND_API_URL = "http://localhost:3002";
+
 async function randomCocktail() {
   try {
-    const response = await axios.get(
-      "https://www.thecocktaildb.com/api/json/v1/1/random.php"
-    );
-    const recipe = response.data.drinks[0];
-    console.log(recipe);
+    const response = await axios.get(`${BACKEND_API_URL}/random_search`);
+    const recipe = response.data;
     return (await recipe);
   } catch (err) {
     console.error(err);
