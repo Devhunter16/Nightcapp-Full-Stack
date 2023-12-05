742a6c24916050ab55c5324b615c4ef8f383a994
Devin Hunter <devhunter16@gmail.com>
2023-12-05 07:47:53 -0700

    Removed unnecessary div from component.

--- frontend/components/recipeCard/RecipeCard.js
+++ frontend/components/recipeCard/RecipeCard.js
@@ -4,27 +4,25 @@ import { BiDrink } from "react-icons/bi";
 
 function RecipeCard({ name, image, ingredients }) {
     return (
-        <div className={styles.container}>
-            <div className={styles.cardContainer}>
-                <h3 className={styles.cocktailName}>{name}</h3>
-                <img src={image} className={styles.cardImage} />
-                <div id={styles.ingredientsBox}>
-                    <div className={styles.ingredientsTitle}>
-                        <BiDrink className={styles.icon} />
-                        <h2>Ingredients</h2>
-                    </div>
-                    <hr className={styles.titleUnderline}></hr>
-                    <ul>
-                        {ingredients.map((ingredient) => (
-                            <li className={styles.recipeIngredient}>
-                                {ingredient.name} -
-                                <span id={styles.measurement}>
-                                    {ingredient.measurement}
-                                </span>
-                            </li>
-                        ))}
-                    </ul>
+        <div className={styles.cardContainer}>
+            <h3 className={styles.cocktailName}>{name}</h3>
+            <img src={image} className={styles.cardImage} />
+            <div id={styles.ingredientsBox}>
+                <div className={styles.ingredientsTitle}>
+                    <BiDrink className={styles.icon} />
+                    <h2>Ingredients</h2>
                 </div>
+                <hr className={styles.titleUnderline}></hr>
+                <ul>
+                    {ingredients.map((ingredient) => (
+                        <li className={styles.recipeIngredient}>
+                            {ingredient.name} -
+                            <span id={styles.measurement}>
+                                {ingredient.measurement}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     );
