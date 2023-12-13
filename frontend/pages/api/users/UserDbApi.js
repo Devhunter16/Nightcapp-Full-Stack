b600736973a4693a2fbe0edb93594f8cc24a1402
Devin Hunter <devhunter16@gmail.com>
2023-12-13 12:45:54 -0700

    Updated route for post request in registerUser method.

---
+++ frontend/pages/api/users/UserDbApi.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BACKEND_API_URL = "http://localhost:3002";
+
+class UserDbApi {
+
+    // token for interactions with the User Database
+    static token;
+
+    // Generic method for making backend api requests
+    static async request(endpoint, data = {}, method = "get") {
+        console.debug("User API Call: ", endpoint, data, method);
+        const url = `${BACKEND_API_URL}${endpoint}`;
+        const headers = {};
+        const params = method === "get" ? data : {};
+
+        try {
+            return (await axios({ url, method, data, params, headers })).data;
+        } catch (err) {
+            console.log(data);
+            console.error("API Error: ", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        };
+    };
+
+    /** Register for Nightcapp */
+    static async registerUser(data) {
+        let response = await this.request(`/auth/register`, data, "post");
+        console.log(data);
+        console.log("signup at backend not working");
+        return response.token;
+    };
+};
+
+export default UserDbApi;
