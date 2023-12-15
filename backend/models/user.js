cfe6361b8f28d6b36444aa7bd4ea2b956a0e6b6e
Devin Hunter <devhunter16@gmail.com>
2023-12-15 15:20:38 -0700

    Wrapped code in a try, catch to handle registration errors.

--- backend/models/user.js
+++ backend/models/user.js
@@ -15,32 +15,38 @@ class User {
     * Throws BadRequestError on duplicates
     */
     static async register({ firstName, lastName, username, password }) {
-        const duplicateCheck = await db.query(
-            `SELECT username
-            FROM users
-            WHERE username = $1`,
-            [username]
-        );
+        try {
+            // Check for duplicate username
+            const duplicateCheck = await db.query(
+                `SELECT username
+               FROM users
+               WHERE username = $1`,
+                [username]
+            );
 
-        if (duplicateCheck.rows[0]) {
-            throw new BadRequestError(`Duplicate username: ${username}`);
+            if (duplicateCheck.rows[0]) {
+                throw new BadRequestError(`Duplicate username: ${username}`);
+            };
+
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 12);
+
+            // Insert user into the database
+            const result = await db.query(
+                `INSERT INTO users
+               (first_name, last_name, username, password)
+               VALUES ($1, $2, $3, $4)
+               RETURNING *`,
+                [firstName, lastName, username, hashedPassword]
+            );
+
+            const user = result.rows[0];
+
+            return user;
+        } catch (error) {
+            // Handle registration errors
+            throw new BadRequestError(`Registration failed: ${error.message}`);
         };
-
-        // The BCRYPT_WORK_FACTOR is the number of rounds the bcrypt algorithm
-        // will execute when hashing the password
-        const hashedPassword = await bcrypt.hash(password, 12);
-
-        const result = await db.query(
-            `INSERT INTO users
-            (first_name, last_name, username, password)
-            VALUES ($1, $2, $3, $4)
-            RETURNING *`,
-            [firstName, lastName, username, hashedPassword]
-        );
-
-        const user = result.rows[0];
-
-        return user;
     };
 };
 
