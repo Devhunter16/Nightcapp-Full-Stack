eb5f29cbae90345c8226088e94b4e6d253b87baa
Devin Hunter <devhunter16@gmail.com>
2023-12-23 15:55:01 -0700

    Added CurrentUserContext.js to provide the currentUser object and setter throughout the app.

---
+++ frontend/components/auth/CurrentUserContext.js
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+
+/** Context - provides currentUser obj and setter throughout app */
+
+const CurrentUserContext = createContext();
+
+export default CurrentUserContext;
