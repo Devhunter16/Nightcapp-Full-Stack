53620752a80c3c8fa061031c0cc4731b811955e8
Devin Hunter <devhunter16@gmail.com>
2023-12-23 15:57:10 -0700

    Added CurrentUser.js

---
+++ frontend/components/auth/CurrentUser.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+import CurrentUserContext from "./CurrentUserContext";
+import useLocalStorage from "../../hooks/useLocalStorage";
+
+export const TOKEN_STORAGE_ID = "nightcapp-token";
+
+export default function CurrentUser({ children }) {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+    // const [token, setToken] = useState(null);
+
+    useEffect(() => {
+        const item = localStorage.getItem(TOKEN_STORAGE_ID);
+        setToken(item);
+    }, [setToken]);
+
+    useEffect(() => {
+        setCurrentUser({ name: "John Doe" });
+    }, []);
+
+    const initialValue = {
+        currentUser,
+        setCurrentUser,
+        token,
+        setToken,
+    };
+
+    return (
+        <CurrentUserContext.Provider value={initialValue}>
+            {children}
+        </CurrentUserContext.Provider>
+    );
+};
