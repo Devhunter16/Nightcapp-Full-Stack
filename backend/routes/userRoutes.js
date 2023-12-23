19c7a8c5060f5036598b11fc579c48d1c713effe
Devin Hunter <devhunter16@gmail.com>
2023-12-23 13:17:03 -0700

    Fixed the login route so that user can successfully log in.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,18 +4,36 @@ const router = express.Router();
 
 const User = require("../models/user");
 
-router.post("/register", async function (req, res, next) {
-    console.log("Registering a user in userRoutes.js");
+router.post("/login", async function (req, res, next) {
+    console.log("Logging a user in in userRoutes.js");
 
     try {
-        const writeObj = { ...req.body };
+        const { username, password } = req.body;
         // const validator = jsonschema.validate(writeObj, userRegisterSchema);
         // if (validator.errors.length != 0) {
         // const errs = validator.errors.map((e) => e.stack);
         // throw new BadRequestError(errs);
         // }
 
-        const newUser = await User.register(writeObj);
+        const user = await User.login(username, password);
+        // const token = createToken(newUser);
+        console.log("User: ", user);
+        return res.status(201).json({ user: user });
+    } catch (err) {
+        return next(err);
+    };
+});
+
+router.post("/register", async function (req, res, next) {
+    try {
+        const data = { ...req.body };
+        // const validator = jsonschema.validate(writeObj, userRegisterSchema);
+        // if (validator.errors.length != 0) {
+        // const errs = validator.errors.map((e) => e.stack);
+        // throw new BadRequestError(errs);
+        // }
+
+        const newUser = await User.register(data);
         // const token = createToken(newUser);
         console.log("New user: ", newUser);
         return res.status(201).json({ user: newUser });
