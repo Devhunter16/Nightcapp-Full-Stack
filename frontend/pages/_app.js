b48f2012968cc36472582a24788c9f21f777a1dc
Devin Hunter <devhunter16@gmail.com>
2023-12-28 18:05:47 -0700

    Added CurrentUserContext component so that all child components could access the properties exported from CurrentUser.js.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,13 @@
 import "@/styles/globals.css";
 
+import CurrentUserContext from "../components/auth/CurrentUser";
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <CurrentUserContext>
+            <Component {...pageProps} />
+        </CurrentUserContext>
+    );
 };
 
 export default MyApp;
