545faf4ecdd201237a848bc91ec3779bfeb5628d
Devin Hunter <devhunter16@gmail.com>
2023-12-29 16:36:43 -0700

    Removed some console.log() statements.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,16 +9,8 @@ const User = require("../models/user");
 router.post("/login", async function (req, res, next) {
     try {
         const { username, password } = req.body;
-        // const validator = jsonschema.validate(writeObj, userRegisterSchema);
-        // if (validator.errors.length != 0) {
-        // const errs = validator.errors.map((e) => e.stack);
-        // throw new BadRequestError(errs);
-        // }
-
         const user = await User.login(username, password);
-        // FIXME CHECK THIS OUT
         const token = createToken(user);
-        console.log("token in userRoutes /login: ", token);
         return res.json({ token });
     } catch (err) {
         return next(err);
@@ -28,15 +20,8 @@ router.post("/login", async function (req, res, next) {
 router.post("/register", async function (req, res, next) {
     try {
         const data = { ...req.body };
-        // const validator = jsonschema.validate(writeObj, userRegisterSchema);
-        // if (validator.errors.length != 0) {
-        // const errs = validator.errors.map((e) => e.stack);
-        // throw new BadRequestError(errs);
-        // }
-
         const newUser = await User.register(data);
         const token = createToken(newUser);
-        console.log("token in userRoutes /register: ", token);
         return res.status(201).json({ token });
     } catch (err) {
         return next(err);
