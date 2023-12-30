f0a366780fa69c044167eeeba84dd749918ce3c5
Devin Hunter <devhunter16@gmail.com>
2023-12-30 12:01:21 -0700

    Added login and logout functionality to the navbar.

--- frontend/components/nav/NavBar.js
+++ frontend/components/nav/NavBar.js
@@ -1,15 +1,19 @@
 import styles from "./Navbar.module.css";
 
+import Link from "next/link";
+import { useContext } from "react";
 import { useRouter } from "next/router";
 
-import Link from "next/link";
-
 import SearchForm from "../searchForm/SearchForm";
+import UserContext from "../auth/CurrentUserContext";
+
+// FIXME Why does the navbar not change no matter what I set display to?
 
 function Navigation() {
     // Only using useRouter to make so that if the user is on the home screen "Home"
     // in the Navbar is highlighted, and the same happens on the "About us" page
     const router = useRouter();
+    const { token, setToken } = useContext(UserContext);
 
     // Conditionally render the SearchForm only on specific pages
     const renderSearchForm = () => {
@@ -26,9 +30,40 @@ function Navigation() {
         return null;
     };
 
-    return (
-        <>
-            <div className={styles.nav}>
+    // Sets token back to null which logs the user out
+    const handleLogout = () => {
+        setToken(null);
+    };
+
+    function loggedInNav() {
+        return (
+            <>
+                <ul className={styles.navList}>
+                    <li className={styles.logo}>
+                        <Link href="/">Nightcapp</Link>
+                    </li>
+                </ul>
+                <div className={styles.searchForm}>
+                    {renderSearchForm()}
+                </div>
+                <ul className={styles.navList}>
+                    <li className={`${styles.navItem} ${router.pathname === "/" ? styles.active : ""}`}>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className={`${styles.navItem} ${router.pathname === "/about" ? styles.active : ""}`}>
+                        <Link href="/about">About Us</Link>
+                    </li>
+                    <li className={`${styles.navItem}`} onClick={handleLogout}>
+                        <Link href="/">Logout</Link>
+                    </li>
+                </ul>
+            </>
+        );
+    };
+
+    function loggedOutNav() {
+        return (
+            <>
                 <ul className={styles.navList}>
                     <li className={styles.logo}>
                         <Link href="/">Nightcapp</Link>
@@ -51,8 +86,14 @@ function Navigation() {
                         <Link href="/register">Register</Link>
                     </li>
                 </ul>
-            </div>
-        </>
+            </>
+        );
+    };
+
+    return (
+        <div className={styles.nav}>
+            {token ? loggedInNav() : loggedOutNav()}
+        </div>
     );
 };
 
