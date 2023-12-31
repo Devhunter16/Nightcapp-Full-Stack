1298dc408cc012e9941ce9aaaf270f130ea539dd
Devin Hunter <devhunter16@gmail.com>
2023-12-31 13:07:35 -0700

    Added logic to greet the user by name upon login.

--- frontend/components/intro/HomePageIntro.js
+++ frontend/components/intro/HomePageIntro.js
@@ -1,13 +1,18 @@
 import styles from "./HomePageIntro.module.css";
 
+import { useContext } from "react";
+
 import SearchForm from "../searchForm/SearchForm";
+import CurrentUserContext from "../auth/CurrentUserContext";
 
 function HomepageIntro() {
+    const { token, currentUser } = useContext(CurrentUserContext);
+
     return (
         <div id={styles.container}>
             <h1 id={styles.h1}>Nightcapp</h1>
             <p id={styles.p}>
-                <span id={styles.welcome}>Welcome in.</span>
+                <span id={styles.welcome}>Welcome {token ? currentUser : "in"}</span>
                 <span>
                     Search for your favorite
                     cocktail or browse our collection.
