2a5375baf0172875195723f3e0c8c213b5263c87
Devin Hunter <devhunter16@gmail.com>
2023-12-31 15:29:56 -0700

    Added logic so that we greet the user by name and so React stops complaining.

--- frontend/components/intro/HomePageIntro.js
+++ frontend/components/intro/HomePageIntro.js
@@ -8,11 +8,18 @@ import CurrentUserContext from "../auth/CurrentUserContext";
 function HomepageIntro() {
     const { token, currentUser } = useContext(CurrentUserContext);
 
+    const getCurrentUser = () => {
+        return token ? currentUser : "in";
+    };
+
+    // Cannot use a function as a React child in the <span>, so we have to do this
+    const userGreeting = getCurrentUser();
+
     return (
         <div id={styles.container}>
             <h1 id={styles.h1}>Nightcapp</h1>
             <p id={styles.p}>
-                <span id={styles.welcome}>Welcome {token ? currentUser : "in"}</span>
+                <span id={styles.welcome}>Welcome {userGreeting}</span>
                 <span>
                     Search for your favorite
                     cocktail or browse our collection.
