d5f4960a31237589317ab2082b6cd26c88c1e1a4
Devin Hunter <devhunter16@gmail.com>
2024-01-05 13:33:31 -0700

    Gave up and simplified this component after hours of wrestling with Next.js over hydration errors.

--- frontend/components/intro/HomePageIntro.js
+++ frontend/components/intro/HomePageIntro.js
@@ -6,23 +6,15 @@ import SearchForm from "../searchForm/SearchForm";
 import CurrentUserContext from "../auth/CurrentUserContext";
 
 function HomepageIntro() {
-    const { token, currentUser } = useContext(CurrentUserContext);
-
-    const getCurrentUser = () => {
-        return token ? currentUser : "in";
-    };
-
-    // Cannot use a function as a React child in the <span>, so we have to do this
-    const userGreeting = getCurrentUser();
-
     return (
         <div id={styles.container}>
             <h1 id={styles.h1}>Nightcapp</h1>
             <p id={styles.p}>
-                <span id={styles.welcome}>Welcome {userGreeting}</span>
+                <span id={styles.welcome}>
+                    Welcome in!
+                </span>
                 <span>
-                    Search for your favorite
-                    cocktail or browse our collection.
+                    Search for your favorite cocktail or browse our collection.
                 </span>
             </p>
             <SearchForm />
