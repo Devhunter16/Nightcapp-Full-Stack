d66c15f297a09f74a674360ce42875c174a1226f
Devin Hunter <devhunter16@gmail.com>
2024-01-05 13:34:56 -0700

    Put the LoggedInNav and LoggedOutNav into their own components.

---
+++ frontend/components/nav/LoggedInNav.js
@@ -0,0 +1,36 @@
+import styles from "./Navbar.module.css";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function LoggedInNav(props) {
+    // Only using useRouter to make so that if the user is on the home screen "Home"
+    // in the Navbar is highlighted, and the same happens on the "About us" page
+    const router = useRouter();
+
+    return (
+        <div className={styles.nav}>
+            <ul className={styles.navList}>
+                <li className={styles.logo}>
+                    <Link href="/">Nightcapp</Link>
+                </li>
+            </ul>
+            <div className={styles.searchForm}>
+                {props.renderSearchForm()}
+            </div>
+            <ul className={styles.navList}>
+                <li className={`${styles.navItem} ${router.pathname === "/" ? styles.active : ""}`}>
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={`${styles.navItem} ${router.pathname === "/about" ? styles.active : ""}`}>
+                    <Link href="/about">About Us</Link>
+                </li>
+                <li className={`${styles.navItem}`} onClick={props.logout}>
+                    Logout
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default LoggedInNav;
---
+++ frontend/components/nav/LoggedOutNav.js
@@ -0,0 +1,39 @@
+import styles from "./Navbar.module.css";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+function LoggedOutNav(props) {
+    // Only using useRouter to make so that if the user is on the home screen "Home"
+    // in the Navbar is highlighted, and the same happens on the "About us" page
+    const router = useRouter();
+
+    return (
+        <div className={styles.nav}>
+            <ul className={styles.navList}>
+                <li className={styles.logo}>
+                    <Link href="/">Nightcapp</Link>
+                </li>
+            </ul>
+            <div className={styles.searchForm}>
+                {props.renderSearchForm()}
+            </div>
+            <ul className={styles.navList}>
+                <li className={`${styles.navItem} ${router.pathname === "/" ? styles.active : ""}`}>
+                    <Link href="/">Home</Link>
+                </li>
+                <li className={`${styles.navItem} ${router.pathname === "/about" ? styles.active : ""}`}>
+                    <Link href="/about">About Us</Link>
+                </li>
+                <li className={`${styles.navItem} ${router.pathname === "/login" ? styles.active : ""}`}>
+                    <Link href="/login">Login</Link>
+                </li>
+                <li className={`${styles.navItem} ${router.pathname === "/register" ? styles.active : ""}`}>
+                    <Link href="/register">Register</Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default LoggedOutNav;
