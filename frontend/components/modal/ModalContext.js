7c17511a9fb07d1acab9cdf69689d466c5ac7586
Devin Hunter <devhunter16@gmail.com>
2024-01-07 07:34:40 -0700

    Added Modal context file so I can use the modal in multiple components without having to pass props.

---
+++ frontend/components/modal/ModalContext.js
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState, useEffect } from 'react';
+
+// Create the context
+export const ModalContext = createContext();
+
+// Create a provider component
+export const ModalProvider = ({ children }) => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [userStatus, setUserStatus] = useState(null);
+    const [primaryMessage, setPrimaryMessage] = useState("");
+    const [secondaryMessage, setSecondaryMessage] = useState("");
+
+    useEffect(() => {
+        if (userStatus === "loggedIn") {
+            handleIsLoggedIn();
+        } else if (userStatus === "loggedOut") {
+            handleIsLoggedOut();
+        };
+        console.log("Hello from Modal useEffect!")
+    }, [userStatus]);
+
+
+    const handleIsLoggedIn = () => {
+        setUserStatus("loggedIn");
+        setIsModalOpen(true);
+        setPrimaryMessage("Welcome back!");
+        setSecondaryMessage("You've been successfully logged in!")
+    };
+
+    const handleIsLoggedOut = () => {
+        setUserStatus("loggedOut");
+        setIsModalOpen(true);
+        setPrimaryMessage("Goodbye!");
+        setSecondaryMessage("You've been successfully logged Out!")
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setUserStatus(null);
+        setIsModalOpen(false);
+    };
+
+    return (
+        <ModalContext.Provider value={{
+            isModalOpen,
+            openModal,
+            closeModal,
+            setUserStatus,
+            primaryMessage,
+            secondaryMessage
+        }}>
+            {children}
+        </ModalContext.Provider>
+    );
+};
+
+// Custom hook to use the modal context
+export const useModal = () => {
+    const context = useContext(ModalContext);
+    if (!context) {
+        throw new Error('useModal must be used within a ModalProvider');
+    }
+    return context;
+};
