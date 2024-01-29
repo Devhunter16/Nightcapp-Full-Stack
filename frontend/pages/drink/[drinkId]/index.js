8bc018efb53317f1afb475e1bb3b672b4d0d15b4
Devin Hunter <devhunter16@gmail.com>
2024-01-28 17:18:20 -0700

    Added handleAddFavorite function to be used in the RecipeDetails component.

--- frontend/pages/drink/[drinkId]/index.js
+++ frontend/pages/drink/[drinkId]/index.js
@@ -1,11 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useRouter } from 'next/router';
 
+import { useModal } from "../../../components/modal/ModalContext";
+import UserContext from "../../../components/auth/CurrentUserContext";
+import UserDbApi from "../../api/users/UserDbApi";
 import matchIngredientsWithMeasurements from "../../../utils/utils.js";
 import Layout from "../../../components/layout/Layout";
 import RecipeDetails from "../../../components/recipeDetails/RecipeDetails";
 
 export default function DrinkPage() {
+    const { setUserStatus } = useModal()
+    const { token, currentUser } = useContext(UserContext);
     const [parsedCocktail, setParsedCocktail] = useState(null);
     const [loading, setLoading] = useState(true);
     const router = useRouter();
@@ -34,11 +39,24 @@ export default function DrinkPage() {
 
                 setParsedCocktail({ name, image, instructions, ingredientsList });
                 setLoading(false);
-            }
+            };
         };
-
         fetchData();
     }, [drink]);
+
+    const handleAddFavorite = async () => {
+        if (token) {
+            const cocktail = JSON.parse(drink);
+            setUserStatus("favoritedRecipe");
+            await UserDbApi.addFavorite({
+                currentUser,
+                cocktail
+            });
+        } else {
+            console.log("No token found.");
+        };
+    };
+
 
     return (
         <Layout>
@@ -50,6 +68,7 @@ export default function DrinkPage() {
                     image={parsedCocktail.image}
                     instructions={parsedCocktail.instructions}
                     ingredientsList={parsedCocktail.ingredientsList}
+                    addFavorite={handleAddFavorite}
                 />
             )}
         </Layout>
