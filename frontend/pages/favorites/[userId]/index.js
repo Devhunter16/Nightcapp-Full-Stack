70fbb89387208e3a3a624e98c3df5c8f3b56e72d
Devin Hunter <devhunter16@gmail.com>
2024-02-01 08:21:15 -0700

    Finally found a way to show either the error message or the favorites list and not one before the other on this page.

--- frontend/pages/favorites/[userId]/index.js
+++ frontend/pages/favorites/[userId]/index.js
@@ -1,3 +1,5 @@
+import styles from "../../../styles/Favorites.module.css";
+
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -6,6 +8,7 @@ import FavoritesList from "../../../components/favoritesList/FavoritesList";
 
 export default function Favorites() {
     const [parsedFavorites, setParsedFavorites] = useState([]);
+    const [showMessage, setShowMessage] = useState(false);
     const router = useRouter();
 
 
@@ -17,14 +20,26 @@ export default function Favorites() {
     useEffect(() => {
         try {
             if (favorites) {
-                const parsedDrinks = JSON.parse(favorites);
-                setParsedFavorites(parsedDrinks);
+                handleSetParsedFavorites();
             };
         } catch (error) {
             console.error("Error parsing favorites data", error);
             // FIXME Handle the error, e.g., redirect to an error page or display an error message
         };
     }, [favorites]);
+
+    const handleSetParsedFavorites = async () => {
+        const parsedDrinks = JSON.parse(favorites);
+        await setParsedFavorites(parsedDrinks);
+        // This strange looking if statement below is the only way of making sure 
+        // the h1 does not flash on screen before showing the favorites list, if
+        // the favorites list length is equal to 2 it essentially means the user
+        // has not added any favorites yet
+        if (favorites.length === 2) {
+            setShowMessage(true);
+        };
+        // ELSE some error message
+    };
 
     const handleShowFavoriteRecipe = (favorite) => {
         // Pushing variables through to page and setting the route
@@ -36,13 +51,15 @@ export default function Favorites() {
         });
     };
 
-
     return (
         <Layout>
-            <FavoritesList
+            {favorites && <FavoritesList
                 favorites={parsedFavorites}
                 handleShowFavorites={handleShowFavoriteRecipe}
-            />
+            />}
+            {showMessage && <h1 className={styles.noFavoritesMessage}>
+                Try searching a cocktail to add it as a favorite!
+            </h1>}
         </Layout>
     );
 };
