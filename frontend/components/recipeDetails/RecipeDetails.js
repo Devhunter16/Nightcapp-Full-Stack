a09a78622b2d6ed21e6d0262b3d8427eb49929f5
Devin Hunter <devhunter16@gmail.com>
2024-02-09 13:17:45 -0700

    Added loading state to create a smoother UX

--- frontend/components/recipeDetails/RecipeDetails.js
+++ frontend/components/recipeDetails/RecipeDetails.js
@@ -9,6 +9,7 @@ function RecipeDetails(props) {
     const { token, currentUser } = useContext(UserContext);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isFavorite, setIsFavorite] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     // Used props.name as a dependency because it changes every time the component
     // mounts and leaving a blank array in order to do the same was causing bugs
@@ -23,10 +24,12 @@ function RecipeDetails(props) {
             if (props.name === favorite.strDrink) {
                 console.log("We have name equivalency");
                 setIsFavorite(true);
+                setIsLoading(false);
                 return;
             } else {
                 console.log("We do not have name equivalency");
                 setIsFavorite(false);
+                setIsLoading(false);
             };
         };
         return;
@@ -42,41 +45,44 @@ function RecipeDetails(props) {
 
     return (
         <>
-            <div id={styles.body}>
-                <img className={styles.img} src={props.image} alt={`${props.name} drink`}></img>
-                <div id={styles.description}>
-                    <h1 id={styles.drinkTitle}>{props.name}</h1>
-                    <h3 id={styles.ingredientsTitle}>Ingredients</h3>
-                    <ul>
-                        <div>
-                            {props.ingredientsList &&
-                                props.ingredientsList.map((ingredient) => (
-                                    <li id={styles.ingredient}>
-                                        {ingredient.name} -{" "}
-                                        <span id={styles.measurements}>
-                                            {ingredient.measurement}
-                                        </span>
-                                    </li>
-                                ))}
-                        </div>
-                    </ul>
-                    <h3 id={styles.directionsTitle}>Directions</h3>
-                    <p id={styles.drinkDescription}>{props.instructions}</p>
-                    {isLoggedIn && (
-                        <>
-                            {isFavorite ? (
-                                <button className={styles.btn} onClick={props.removeFavorite}>
-                                    Remove from my favorites
-                                </button>
-                            ) : (
-                                <button className={styles.btn} onClick={props.addFavorite}>
-                                    Add to my favorites
-                                </button>
-                            )}
-                        </>
-                    )}
+            {isLoading ? (
+                <p>Loading...</p>) : (
+                <div id={styles.body}>
+                    <img className={styles.img} src={props.image} alt={`${props.name} drink`}></img>
+                    <div id={styles.description}>
+                        <h1 id={styles.drinkTitle}>{props.name}</h1>
+                        <h3 id={styles.ingredientsTitle}>Ingredients</h3>
+                        <ul>
+                            <div>
+                                {props.ingredientsList &&
+                                    props.ingredientsList.map((ingredient) => (
+                                        <li id={styles.ingredient}>
+                                            {ingredient.name} -{" "}
+                                            <span id={styles.measurements}>
+                                                {ingredient.measurement}
+                                            </span>
+                                        </li>
+                                    ))}
+                            </div>
+                        </ul>
+                        <h3 id={styles.directionsTitle}>Directions</h3>
+                        <p id={styles.drinkDescription}>{props.instructions}</p>
+                        {isLoggedIn && (
+                            <>
+                                {isFavorite ? (
+                                    <button className={styles.btn} onClick={props.removeFavorite}>
+                                        Remove from my favorites
+                                    </button>
+                                ) : (
+                                    <button className={styles.btn} onClick={props.addFavorite}>
+                                        Add to my favorites
+                                    </button>
+                                )}
+                            </>
+                        )}
+                    </div>
                 </div>
-            </div>
+            )}
         </>
     );
 };
