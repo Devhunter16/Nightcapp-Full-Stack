7d2fd4ef96a0f94077a6eca7900de8929d9d2b77
Devin Hunter <devhunter16@gmail.com>
2024-03-08 12:08:47 -0700

    Changed db.js file to reflect the appropriate .env file values.

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,20 @@
-const { Client } = require('pg')
+const { Client } = require('pg');
 require('dotenv').config();
 
 const db = new Client({
     user: process.env.DB_USER,
-    host: "localhost",
-    database: "nightcapp",
+    host: process.env.DB_ENDPOINT,
+    database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: process.env.DEV_PORT,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false // For testing purposes, you can set this to false
+    },
+    connectionTimeoutMillis: 5000 // Optional: Set connection timeout
 });
 
 db.connect()
     .then(() => console.log('Connected to the database'))
-    .catch(err => console.error('Error connecting to the database', err));
+    .catch(err => console.error('Error connecting to the database:', err.stack));
 
 module.exports = db;
