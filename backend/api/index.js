22aaee79989034081fc48d8a4ba96ff2de87569a
Devin Hunter <devhunter16@gmail.com>
2024-03-09 15:31:46 -0700

    Apparently you need this structure so vercel stops comlaining about deploying your app.

---
+++ backend/api/index.js
@@ -0,0 +1,33 @@
+/** Express app for nightcapp */
+const express = require("express");
+// CORS is important because our backend and frontend are running on different ports
+const cors = require("cors");
+
+const cocktailDbRoutes = require(".././routes/cocktailDbRoutes");
+const userRoutes = require(".././routes/userRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/cocktaildb", cocktailDbRoutes);
+app.use("/auth", userRoutes);
+
+/** Handle 404 errors */
+app.use(function (req, res, next) {
+    res.status(404).json({ error: "Not Found" });
+});
+
+/** Generic error handler; this middleware should log anything that hits it to the console */
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
+
+module.exports = app;
