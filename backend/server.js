7085ddd40f11e16cce1cbddb23d14458284fd920
Devin Hunter <devhunter16@gmail.com>
2024-03-08 17:29:07 -0700

    Changed PORT variable to be compatible with the deployment.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = 3002;
+const PORT = process.env.PORT || 3002; // Use the provided port by Vercel or default to 3002
 
 app.listen(PORT, function () {
     console.log(`Listening on http://localhost:${PORT}`);
